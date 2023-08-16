1de7f7285af08667debdcb44ed2b7808da39ecc9
Alisson <alissonalves64@hotmail.com>
2023-08-16 00:19:03 -0300

    Iniciando beckend da aplicação

---
+++ src/controllers/usersController.js
@@ -0,0 +1,13 @@
+const AppError = require("../utils/Apperror")
+
+class UsersController {
+ create (request, response) {
+   const {name, email, password} = request.body;
+   if(!name) {
+    throw new AppError("O nome é obrigatório!");
+   }
+   response.status(201).json({ name, email, password });
+  }
+}
+
+module.exports = UsersController
