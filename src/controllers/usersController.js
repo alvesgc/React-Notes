d6fcba846244f34bc9dc331290a842dd8b1211ed
Alisson <alissonalves64@hotmail.com>
2023-08-26 14:01:20 -0300

    feat: adcionando a lib knex

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,73 +3,79 @@ const AppError = require("../utils/Apperror");
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
- async create (request, response) {
-   const {name, email, password} = request.body;
-
-   const database = await sqliteConnection();
-   const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
-
-   if(checkUserExist){
-    throw new AppError("Este e-mail já está em uso.")
-  }
- 
-
- const hashedPassword = await hash(password, 8)
-
-   await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, hashedPassword]
-    )
-
-
-
-   return response.status(201).json();
-  }
-
-  async update(request, response){
-    const { name, email, password, old_password} = request.body;
-    const { id } = request.params;
+  async create(request, response) {
+    const { name, email, password } = request.body;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
-    if(!user) {
-      throw new AppError("Usuário não encontrado")
-    }
+    const checkUserExist = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
 
-    const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
-
-    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+    if (checkUserExist) {
       throw new AppError("Este e-mail já está em uso.");
     }
 
-    user.name = name;
-    user.email = email;
+    const hashedPassword = await hash(password, 8);
 
-    if(password && !old_password) {
-      throw new AppError("Você informar a senha antiga para definir a nova senha")
-     }
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    );
 
-     if(password && old_password) {
-      const checkOldPassword = await compare(old_password, user.password)
+    return response.status(201).json();
+  }
 
-      if(!checkOldPassword) {
-        throw new AppError("A senha antiga não confere.")
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdateEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Você informar a senha antiga para definir a nova senha"
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.");
       }
 
-      user.password = await hash(password, 8)
-     }
+      user.password = await hash(password, 8);
+    }
 
-     await database.run(`
+    await database.run(
+      `
       UPDATE users SET
       name = ?,
       email = ?,
       password = ?,
-      updated_at = ?
-      WHERE id = ?`, 
-      [user.name, user.email, user.password, new Date(), id]
-    )
-    return response.status(200).json()
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    );
+    return response.status(200).json();
   }
 }
 
-module.exports = UsersController
+module.exports = UsersController;
