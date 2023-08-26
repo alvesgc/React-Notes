13e9c8ca068c2ef255248b88b69cafde0a7840bb
Alisson <alissonalves64@hotmail.com>
2023-08-26 18:40:41 -0300

    feat: add tags and link table

---
+++ src/database/knex/migrations/20230826213439_creatLinks.js
@@ -0,0 +1,10 @@
+exports.up = (knex) =>
+  knex.schema.createTable("links", (table) => {
+    table.increments("id");
+    table.text("url").notNullable();
+    
+    table.integer("note_id").references("notes").inTable("users").onDelete("CASCADE");
+    table.timestamp("updated_at").default(knex.fn.now())
+  });
+
+exports.down = (knex) => knex.schema.dropTable("links", (table) => {});
